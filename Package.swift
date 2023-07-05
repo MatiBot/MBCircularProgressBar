1dd2f3d94c6b4357e3b0e7f47c60614bb981c50c
Normen <nk@fluidmobile.de>
2023-07-05 16:55:08 +0200

    Add publicHeadersPath to the target to allow interop with objc

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "MBCircularProgressBar",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
         .library(name: "MBCircularProgressBar", targets: ["MBCircularProgressBar"])
     ],
@@ -10,7 +13,8 @@ let package = Package(
         .target(
             name: "MBCircularProgressBar",
             path: "Sources",
-            exclude: ["Example"]
+            exclude: ["Example"],
+            publicHeadersPath: "."
         ),
     ]
 )
